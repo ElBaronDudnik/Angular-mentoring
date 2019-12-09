ae02c96cbc9a13a343267dd5d43c47c41c3a980c
Olena Dudnyk <olena.dudnyk@thomsonreuters.com>
2019-12-09 11:29:18 +0200

    Merge confcits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login-page/login/login.component';
-
 
 const routes: Routes = [
     { path: 'courses',
       loadChildren: () => import('./courses-page/courses-page.module').then(m => m.CoursesPageModule)
-    },
-    { path: 'login',
-      component: LoginComponent
     },
     { path: '',
       redirectTo: 'login',
